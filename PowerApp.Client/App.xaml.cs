070194223e4d264d968f7574c86e3816bab6c148
Martin Kirk <mk@autoit.dk>
2025-07-12 10:55:18 +0200

    det blir godt

--- PowerApp.Client/App.xaml.cs
+++ PowerApp.Client/App.xaml.cs
@@ -1,17 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Security.Principal;
 using System.Windows;
 
 namespace PowerApp.Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!IsRunAsAdministrator())
+            {
+                // Restart the application with administrator rights
+                var processInfo = new ProcessStartInfo
+                {
+                    FileName = Process.GetCurrentProcess().MainModule.FileName!,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                };
+
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch
+                {
+                    // User refused the elevation
+                    Shutdown();
+                    return;
+                }
+
+                Shutdown();
+                return;
+            }
+        }
+
+        private static bool IsRunAsAdministrator()
+        {
+            if (Debugger.IsAttached)
+            {
+                // If debugging, we assume the user has the necessary permissions
+                return true;
+            }
+
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
     }
 }
